d16255dff2633a1f1a7479a062a2e12e54115183
JhonTryton <jhonskyload@gmail.com>
2025-02-28 23:29:52 +0000

    Delete network_scanner.py

--- network_scanner.py
+++
@@ -1,8 +0,0 @@
-import nmap
-
-def scan_network(network):
-    scanner = nmap.PortScanner()
-    scanner.scan(hosts=network, arguments="-sn")  # Scan sans port
-    active_hosts = [host for host in scanner.all_hosts()]
-    return active_hosts
-  
